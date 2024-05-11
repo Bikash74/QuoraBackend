2c2136851682acb7b717c332d61583ff83087e03
Bikash74 <shaw46482@gmail.com>
2024-05-11 19:54:11 +0530

    set up and connenct to  MySql database

---
+++ src/main/java/org/example/quorabackend/QuoraBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.quorabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoraBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuoraBackendApplication.class, args);
+    }
+
+}
